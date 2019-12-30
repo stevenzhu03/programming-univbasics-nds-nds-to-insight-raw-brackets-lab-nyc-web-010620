53b8fca70aaf1d502501ba0971fd504f2a3748b7
Steven Zhu <stevenzhu03@gmail.com>
2019-12-30 18:42:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,18 +5,18 @@ def directors_totals(nds)
   dir_index = 0
   totals = {}
 
-  while dir_index < nds.length {
+  while dir_index < nds.length do
     di_name = nds[dir_index][:name]
     totals[dir_name] = 0
     movie_index = 0
 
-    while movie_index < nds[dir_index][:movies].length {
+    while movie_index < nds[dir_index][:movies].length do
       totals[dir_name] += nds[dir_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
-    }
+    end
 
     dir_index += 1
-  }
+  end
 
   totals
 end
